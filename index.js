e8ad3010622ad4368b8c7562132998d27c1d4ab9
Robin <robin.pajula@gmail.com>
2026-09-05 11:42:21 +0300

    Read data from file

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
+const fs = require("fs");
 
-const path = require('path')
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+const path = require("path");
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => {
-  res.render("index")
-})
+app.get("/", (req, res) => {
+  fs.readFile("./tasks", "utf8", (err, data) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    const tasks = data.split("\r\n");
+    res.render("index", { tasks: tasks });
+  });
+});
 
 app.listen(3001, () => {
-  console.log('Example app is started at http://localhost:3001')
-})
+  console.log("Example app is started at http://localhost:3001");
+});
