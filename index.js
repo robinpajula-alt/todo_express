cb1ec48b1757942842983440f2feff66fdf012f6
Robin <robin.pajula@gmail.com>
2026-09-05 12:33:58 +0300

    Write data to file

--- index.js
+++ index.js
@@ -6,14 +6,38 @@ const path = require("path");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+const readFile = (filename) => {
+  return new Promise((resolve, reject) => {
+    fs.readFile("./tasks", "utf8", (err, data) => {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      const tasks = data.split("\r\n");
+      resolve(tasks);
+    });
+  });
+};
+
 app.get("/", (req, res) => {
-  fs.readFile("./tasks", "utf8", (err, data) => {
-    if (err) {
-      console.log(err);
-      return;
-    }
-    const tasks = data.split("\r\n");
+  readFile("./tasks").then((tasks) => {
+    console.log(tasks);
     res.render("index", { tasks: tasks });
+  });
+});
+app.use(express.urlencoded({ extended: true }));
+
+app.post("/", (req, res) => {
+  readFile("./tasks").then((tasks) => {
+    tasks.push(req.body.task);
+    const data = tasks.join("\r\n");
+    fs.writeFile("./tasks", data, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      res.redirect("/");
+    });
   });
 });
 
